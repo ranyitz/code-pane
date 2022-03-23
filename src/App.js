0c8f318570b558a70e3d1c665c838d394afa9b94
Ran Yitzhaki <ranyitz@gmail.com>
2022-03-23 02:13:49 +0200

    initial POC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,97 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import tomorrow from "react-syntax-highlighter/dist/cjs/styles/prism/tomorrow";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import domtoimage from "dom-to-image";
+import { saveAs } from "file-saver";
+
+const codeString = `import reportWebVitals from './reportWebVitals';
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+  
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals();`;
+
+const Component = () => {
+  const [code, setCode] = useState(codeString);
+
+  return (
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <textarea
+          style={{ width: "800px", height: "300px" }}
+          height="500"
+          value={code}
+          onChange={(e) => setCode(e.currentTarget.value)}
+        />
+      </div>
+      <div
+        id="export-container"
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <SyntaxHighlighter
+          language="javascript"
+          id="code"
+          style={tomorrow}
+          wrapLines={true}
+          showLineNumbers={true}
+          lineProps={(number) => {
+            if (number === 7 || number === 1) return { style: { opacity: 1 } };
+            return { style: { opacity: 0.3 } };
+          }}
+        >
+          {code}
+        </SyntaxHighlighter>
+      </div>
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Syntax highlighter</h1>
+      <button
+        onClick={async () => {
+          const node = document.getElementById("export-container");
+          const exportSize = 4;
+
+          const width = node.offsetWidth * exportSize;
+          const height = node.offsetHeight * exportSize;
+
+          const png = await domtoimage.toPng(node, {
+            style: {
+              transform: `scale(${exportSize})`,
+              "transform-origin": "center",
+              background: "none",
+            },
+            width,
+            height,
+          });
+
+          saveAs(png, "my-node.png");
+        }}
+      >
+        Export
+      </button>
+      <Component />
     </div>
   );
 }
-
 export default App;
